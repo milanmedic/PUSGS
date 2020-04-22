cb468cf724df4c8c59280659c426a6252ef3ca77
Milan Medic <mmedic596@gmail.com>
2020-04-22 17:52:21 +0200

    Added BaseSelect, BaseButton & BaseForm components

---
+++ frontend/src/components/BaseComponents/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button v-on="$listeners" v-bind="$attrs" class="button" :class="buttonClass">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    buttonClass: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/BaseComponents/BaseForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form v-on="$listeners" class="base-form" :class="formClass">
+    <slot name="formFields"></slot>
+    <slot name="formButtons"></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    formClass: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+</style>
