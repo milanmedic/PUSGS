d7b8b8e36972c88e096f714202c84fb28e7c9d52
Milan Medic <mmedic596@gmail.com>
2020-04-22 03:11:51 +0200

    Added Base Input Component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BaseInput label="Name" v-model="name" type="text" placeholder="Name" class="field" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BaseInput from "@/components/BaseComponents/BaseInput.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    BaseInput
   }
 };
 </script>
