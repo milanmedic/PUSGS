0cd8665530e47fcb497766fb4050feaa01500ba6
Milan Medic <mmedic596@gmail.com>
2020-04-24 18:49:12 +0200

    Styled ProfilePageHeader component

--- frontend/src/components/SharedComponents/ProfilePageComponents/ProfilePageHeader.vue
+++ frontend/src/components/SharedComponents/ProfilePageComponents/ProfilePageHeader.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="profile-page-header">
     <figure>
-      <img src="@/assets/heroAvatar.svg" alt="heroAvatar" />
+      <img src="@/assets/heroAvatar.svg" alt="heroAvatar" id="profile-picture" />
     </figure>
     <div class="basic-information">
-      <p id="user-name">{{userData.name}}</p>
-      <p id="user-location">{{userData.description}}</p>
+      <h2 id="user-name">{{userData.name}}</h2>
+      <h3 id="user-name">{{userData.username}}</h3>
+      <p id="user-location">{{userData.location}}</p>
     </div>
     <div class="profile-description">
       <p class="user-description">{{userData.description}}</p>
@@ -19,6 +20,7 @@ export default {
     return {
       userData: {
         name: "Luke Skywalker",
+        username: "@kosovo1244",
         location: "Pambukovica, SFRJ",
         description:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis labore perferendis nostrum sit mollitia! Dolore, error doloribus possimus voluptatibus cumque illum quod ullam quas! Dolore magnam sapiente voluptas a odit."
@@ -29,4 +31,58 @@ export default {
 </script>
 
 <style>
+.profile-page-header {
+  width: 95%;
+  padding: 2.5%;
+  height: 95%;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 5%;
+  background-color: #6e89eb;
+  border-radius: 5px;
+}
+
+.profile-page-header figure {
+  width: 100%;
+  height: 100%;
+  grid-column: 1/2;
+}
+#profile-picture {
+  max-width: 100%;
+  max-height: 100%;
+}
+.basic-information {
+  width: 97.5%;
+  grid-column-start: 2;
+  grid-column-end: 3;
+  background-color: white;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-left: 2.5%;
+}
+
+.basic-information > h2 {
+  font-size: 1.2rem;
+}
+
+.basic-information > p,
+.basic-information > h3 {
+  color: rgba(0, 0, 0, 0.5);
+}
+
+.profile-description {
+  width: 100%;
+  grid-column: 1/3;
+  grid-row: 2;
+  background-color: white;
+  border-radius: 5px;
+}
+
+.profile-description > p {
+  font-size: 0.9rem;
+  padding: 2.5%;
+}
 </style>
--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>User Profile</div>
+  <BaseProfile class="user-profile">
+    <template v-slot:profileHeader>
+      <ProfilePageHeader />
+    </template>
+    <template v-slot:tabMenu>
+      <div>Tab Menu</div>
+    </template>
+    <template v-slot:tabList>
+      <div>Tab List</div>
+    </template>
+  </BaseProfile>
 </template>
 
 <script>
 import BaseProfile from "@/components/BaseComponents/BaseProfile.vue";
-export default {};
+import ProfilePageHeader from "@/components/SharedComponents/ProfilePageComponents/ProfilePageHeader.vue";
+export default {
+  components: {
+    BaseProfile,
+    ProfilePageHeader
+  }
+};
 </script>
 
 <style>
+.user-profile {
+  position: relative;
+  height: 90%;
+  width: 95%;
+  margin: 10% 2.5% 0 2.5%;
+  display: grid;
+  grid-template-rows: 2fr 1fr 3fr;
+  grid-gap: 5%;
+}
 </style>
