8e52275c0609445cc20263847fdaa29d2a60070b
Milan Medic <mmedic596@gmail.com>
2020-08-12 17:15:06 +0200

    Added additional loggers

--- server/src/services/utilities/logging/index.js
+++ server/src/services/utilities/logging/index.js
@@ -3,10 +3,14 @@ const { format } = winston
 const { combine, label, printf, timestamp, json } = format
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`
+    return `
+    ----------------------------------------------------------
+    [${timestamp}] [${label}] (${level.toUpperCase()}) : ${message}
+    ----------------------------------------------------------
+    `
 })
 
-winston.loggers.add('authController', {
+winston.loggers.add('AuthController', {
     format: combine(
         label({ label: 'Authentication Controller' }),
         timestamp(),
@@ -15,6 +19,38 @@ winston.loggers.add('authController', {
     transports: [
         new winston.transports.Console({}),
         new winston.transports.File({ filename: 'authController.log' }),
+    ],
+})
+
+winston.loggers.add('OAuthController', {
+    format: combine(
+        label({ label: 'OAuth Controller' }),
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({ filename: 'oauthController.log' }),
+    ],
+})
+
+winston.loggers.add('UserController', {
+    format: combine(label({ label: 'User Controller' }), timestamp(), myFormat),
+    transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({ filename: 'userController.log' }),
+    ],
+})
+
+winston.loggers.add('AdminController', {
+    format: combine(
+        label({ label: 'Admin Controller' }),
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({ filename: 'AdminController.log' }),
     ],
 })
 
